f8e5daa9c1a42a8b0daac2974fb777cc4b6fd8d7
Diego Coello <diego.coello.lopez@gmail.com>
2026-02-10 10:06:20 +0100

    Redirección de archivos

---
+++ src/main/java/com/canaryshop/canaryshop/CanaryshopApplication.java
@@ -0,0 +1,13 @@
+package com.canaryshop.canaryshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CanaryshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CanaryshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/canaryshop/canaryshop/CanaryshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.canaryshop.canaryshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CanaryshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
