1665f11ba7e241517424a4cd355ba8b72329e16f
misper <victoriuscamver@gmail.com>
2026-02-19 17:56:34 +0100

    Created order entity

---
+++ src/main/java/com/canaryshop/canaryshop/Order.java
@@ -0,0 +1,50 @@
+package com.canaryshop.canaryshop;
+
+import jakarta.persistence.*;
+
+import java.util.HashMap;
+import java.util.Set;
+import java.util.Map;
+
+
+@Entity
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private double price = 0F;
+    @OneToMany
+    private final Map<Product, Integer> products = new HashMap<>();
+
+    public Order(){}
+    public Long getId() {
+        return id;
+    }
+    public double getPrice(){
+        return price;
+    }
+    public Set<Product> getProducts(){
+        return products.keySet();
+    }
+    public int getProductQuantity(Product product){
+        return this.products.get(product);
+    }
+    public void addProduct(Product product){
+        if (products.containsKey(product)) { return; }
+        products.put(product, 1);
+        price+=product.getPrice();
+    }
+    public void removeProduct(Product product){
+        if (!products.containsKey(product)){ return; }
+        int amount = products.get(product);
+        price-=product.getPrice()*amount;
+        products.remove(product);
+    }
+    public void setAmount(Product product, int amount){
+        amount = Math.min(amount, product.getStock());
+        price-=product.getPrice()*products.get(product);
+        price+=product.getPrice()*amount;
+        products.replace(product, amount);
+    }
+}
