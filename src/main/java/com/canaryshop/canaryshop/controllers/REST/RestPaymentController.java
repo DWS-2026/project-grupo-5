21b5b90debcd9db3a203ff280b5772b38d98cd79
JCL625 <jorgecrespo625@gmail.com>
2026-05-06 18:45:30 +0200

    Fixed errors in the payment api and in the user view

--- src/main/java/com/canaryshop/canaryshop/controllers/REST/RestPaymentController.java
+++ src/main/java/com/canaryshop/canaryshop/controllers/REST/RestPaymentController.java
@@ -7,23 +7,21 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.canaryshop.canaryshop.DTOs.OrderDTO;
 import com.canaryshop.canaryshop.DTOs.OrderMapper;
+import com.canaryshop.canaryshop.DTOs.StringDTO;
 import com.canaryshop.canaryshop.entities.Order;
-import com.canaryshop.canaryshop.entities.Product;
 import com.canaryshop.canaryshop.entities.User;
 import com.canaryshop.canaryshop.services.OrderService;
-import com.canaryshop.canaryshop.services.ProductService;
 import com.canaryshop.canaryshop.services.UserService;
 
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
@@ -54,7 +52,8 @@ public class RestPaymentController {
         if (cart.getProducts().isEmpty()) {
             throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Cart is empty");
         }
-        this.orderService.closeOrder(user, cart,this.orderService.getDiscount(code.str()));
+        String discountCode = (code != null) ? code.str() : null;
+        this.orderService.closeOrder(user, cart,this.orderService.getDiscount(discountCode));
         return ResponseEntity.ok(orderMapper.toDTO(cart));
     }
 
@@ -63,11 +62,12 @@ public class RestPaymentController {
             @ApiResponse(responseCode = "200", description = "The product have been paid", content = @Content(mediaType = "application/json", schema = @Schema(implementation = OrderDTO.class))),
             @ApiResponse(responseCode = "404", description = "Could not found the product", content = @Content)
     })
-    @PostMapping("/{id}")
+    @PostMapping("/{productID}")
     public ResponseEntity<OrderDTO> payProduct(@AuthenticationPrincipal UserDetails userDetails, @PathVariable long productID, @RequestBody(required = false) StringDTO code) {
         User user = userService.getUser(userDetails.getUsername());
         Order temp = this.orderService.createTempCart(user, productID);
-        this.orderService.closeOrder(user, temp, this.orderService.getDiscount(code.str()));
+        String discountCode = (code != null) ? code.str() : null;
+        this.orderService.closeOrder(user, temp, this.orderService.getDiscount(discountCode));
         return ResponseEntity.ok(orderMapper.toDTO(temp));
     }
 }
